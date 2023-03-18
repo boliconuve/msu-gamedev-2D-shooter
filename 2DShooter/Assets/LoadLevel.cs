ca5e6df67c2212c538ef7ac4939a181f96baee4b
Víctor Fuente <vitxoren@gmail.com>
2023-03-18 16:53:32 +0100

    First Commit - 2D Shooter

---
+++ 2DShooter/Assets/LoadLevel.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadLevel : MonoBehaviour
+{
+    public BoxCollider2D collider2D; //Collider2D
+
+    
+}
